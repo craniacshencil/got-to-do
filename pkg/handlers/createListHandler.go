a2b0918ff6d023aa8f8b65fa74272fe3862690ec
craniacshencil <rajnikamrajnikam@gmail.com>
2024-07-12 18:46:34 +0530

    Add validation while creating todos
    - Makes sure that timings don't clash
    Eg:
    Start-time of task 1: 9:30
    End-time of task 1: 10:00
    Start-time of task 2: 9:45

--- pkg/handlers/createListHandler.go
+++ pkg/handlers/createListHandler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -33,11 +34,60 @@ func (ApiConfig *ApiCfg) CreateListHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	// Parse the json
-	var Todo map[string]Task
+	var Todo map[int]Task
 	err = utils.ParseJSON(r, &Todo)
 	if err != nil {
 		log.Println("ERR: While parsing Todo JSON", err)
 		utils.WriteJSON(w, http.StatusInternalServerError, err.Error())
 		return
 	}
+
+	// Validate the timings make sure there is no clash
+	// Eg: One task starts and ends at: 13:00 to 14:00, if other task starts at 13:30 then error should be reported
+	err = validateTimings(Todo)
+	if err != nil {
+		log.Println(err)
+		utils.WriteJSON(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
+func validateTimings(todo map[int]Task) error {
+	// TODO: Primary validation
+	// Making sure that all the tasks have endtime > starttime
+	// Making sure all the tasks are of proper type(i think using a struct basically makes sure that this is taken care of)
+	var start, end time.Time
+	var i, j, startVal, endVal int
+	var startTimeArr, endTimeArr []int
+	for _, task := range todo {
+		start = task.StartTime
+		end = task.EndTime
+
+		// getting time in form: 13:00 to 14:00 -> 1300, 1400
+		startVal = start.Hour()*1000 + start.Minute()
+		endVal = end.Hour()*1000 + end.Minute()
+
+		startTimeArr = append(startTimeArr, startVal)
+		endTimeArr = append(endTimeArr, endVal)
+	}
+	for i = 0; i < len(startTimeArr); i++ {
+		for j = 0; j < len(startTimeArr); j++ {
+			if i == j {
+				continue
+			}
+			// Condition says that if startTime of a task lies in time assigned for other task
+			if startTimeArr[i] < endTimeArr[j] && startTimeArr[i] >= startTimeArr[j] {
+				return fmt.Errorf(
+					"starting time of task %d: %d\nend time of task %d: %d\nstarting time of task %d: %d\nThis is clashing",
+					i,
+					startTimeArr[i],
+					i,
+					endTimeArr[i],
+					j,
+					startTimeArr[j],
+				)
+			}
+		}
+	}
+	return nil
 }
